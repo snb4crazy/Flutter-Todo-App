5e6f869019666aa6b69cda4c01cdd65fc3e90c08
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 16:06:16 -0400

    Add Tasks Screen: Base Structure

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Add a task',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(),
+            TextButton(onPressed: () {}, child: Icon(Icons.add))
+            //Button
+          ],
+        ),
+      ),
+    );
+  }
+}
