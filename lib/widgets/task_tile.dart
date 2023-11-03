92fd9547a0e622dbdd28de2883db7e90421c1c7b
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 17:11:27 -0400

    TaskTile classes Statefull & Stateless widgets

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: TaskCheckBox(isChecked),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+
+  TaskCheckBox(this.checkBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: (newValue) {},
     );
   }
 }
