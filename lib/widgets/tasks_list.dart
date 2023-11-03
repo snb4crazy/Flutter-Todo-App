e8d0e10aad2996bebbdd2b027a708db0fc181b22
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 18:05:43 -0400

    Tasks List hardcoded experiments

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
+import 'package:todoey/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'task #1'),
+    Task(name: 'task #2'),
+    Task(name: 'task #3'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        TaskTile(),
-        ListTile(
-          title: Text('Another hardcoded (!) task'),
-          trailing: Checkbox(
-            value: false,
-            onChanged: (bool? value) {},
-          ),
-        )
+        TaskTile(isChecked: tasks[0].isDone, title: tasks[0].name),
+        TaskTile(isChecked: tasks[1].isDone, title: tasks[1].name),
+        TaskTile(isChecked: tasks[2].isDone, title: tasks[2].name),
       ],
     );
   }
