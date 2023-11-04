ce0427bc4c18a39dc3ded422c5af6e21513e4e39
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-04 14:17:31 -0400

    Integrate ChangeNotifierProvider with the Main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/tasks_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      //builder: (context) => TaskData(),
+      create: (BuildContext context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
