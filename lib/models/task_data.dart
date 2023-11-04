80e9ecfd17d6ee53f50c08b75a1798e7b45df63a
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 21:30:57 -0400

    Create TaskData class extending ChangeNotifier

---
+++ lib/models/task_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'task #1'),
+    Task(name: 'task #2'),
+    Task(name: 'task #3'),
+  ];
+}
