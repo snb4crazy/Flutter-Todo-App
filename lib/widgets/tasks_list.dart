50fff84a393d3dfb56d5db1b38566862fbb67ed9
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 21:30:00 -0400

    Integrate Tasks List & Provider

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
-import 'package:todoey/models/task.dart';
+import 'package:todoey/models/task_data.dart';
+import 'package:provider/provider.dart';
 
 class TasksList extends StatefulWidget {
-  final List<Task> tasks;
-
-  TasksList({required this.tasks});
   @override
   _TasksListState createState() => _TasksListState();
 }
@@ -16,15 +14,15 @@ class _TasksListState extends State<TasksList> {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TaskTile(
-            isChecked: widget.tasks[index].isDone,
-            title: widget.tasks[index].name,
+            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
+            title: Provider.of<TaskData>(context).tasks[index].name,
             checkboxCallback: (bool? checkboxState) {
               setState(() {
-                widget.tasks[index].toggleDone();
+                //widget.tasks[index].toggleDone();
               });
             });
       },
-      itemCount: widget.tasks.length,
+      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
