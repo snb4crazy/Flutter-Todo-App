eb9faef00c602c1da21bfcc4c5f5b5a6e6ccfb38
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 21:42:56 -0400

    Integrate Tasks List widget with Consumer

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,18 +6,22 @@ import 'package:provider/provider.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            title: Provider.of<TaskData>(context).tasks[index].name,
-            checkboxCallback: (bool? checkboxState) {
-              //setState(() {
-              //widget.tasks[index].toggleDone();
-              //});
-            });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+                isChecked: taskData.tasks[index].isDone,
+                title: taskData.tasks[index].name,
+                checkboxCallback: (bool? checkboxState) {
+                  //setState(() {
+                  //widget.tasks[index].toggleDone();
+                  //});
+                });
+          },
+          itemCount: taskData.tasks.length,
+        );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
