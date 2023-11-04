46bef8c67ef11bf50dcc3edf74e71edead03b4b9
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-03 21:37:21 -0400

    Refactoring Tasks List - make Stateless class

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -3,12 +3,7 @@ import 'package:todoey/widgets/task_tile.dart';
 import 'package:todoey/models/task_data.dart';
 import 'package:provider/provider.dart';
 
-class TasksList extends StatefulWidget {
-  @override
-  _TasksListState createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -17,9 +12,9 @@ class _TasksListState extends State<TasksList> {
             isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
             title: Provider.of<TaskData>(context).tasks[index].name,
             checkboxCallback: (bool? checkboxState) {
-              setState(() {
-                //widget.tasks[index].toggleDone();
-              });
+              //setState(() {
+              //widget.tasks[index].toggleDone();
+              //});
             });
       },
       itemCount: Provider.of<TaskData>(context).tasks.length,
